51dc864022dce69c0489cada2d123d3cff160830
Brian <brian.magana2929@alumnos.udg.mx>
2024-02-15 13:29:56 +0100

    Commit Inicial

---
+++ analizador_lexico.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class AnalizadorLexico:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("Analizador Léxico")
+        self.root.geometry("800x405")
+
+        self.root.mainloop()
+
+
+AnalizadorLexico()
